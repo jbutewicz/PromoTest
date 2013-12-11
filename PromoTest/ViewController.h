73427543cdd1c7cab4818bb8088157b73eb91631
Joseph Butewicz <jbutewicz@gmail.com>
2013-12-11 11:03:43 -0500

    Final commit
    Oops, never committed the final changes. Here they are!

--- PromoTest/ViewController.h
+++ PromoTest/ViewController.h
@@ -8,6 +8,10 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UITextFieldDelegate> {
+    
+}
+
+@property (weak, nonatomic) IBOutlet UITextView *textView;
 
 @end
--- PromoTest/ViewController.m
+++ PromoTest/ViewController.m
@@ -7,12 +7,94 @@
 //
 
 #import "ViewController.h"
+#import "MBProgressHUD.h"
 
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    
+    // Get device unique ID
+    UIDevice *device = [UIDevice currentDevice];
+    NSString *uniqueIdentifier = [[device identifierForVendor] UUIDString];
+    
+    // Store code from UI
+    NSString *code = textField.text;
+    
+    // Hide keyword
+    [textField resignFirstResponder];
+
+    // Clear text field
+    _textView.text = @"";
+    
+    // Show progress window
+    MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:self.view animated:YES];
+    hud.labelText = @"Redeeming code...";
+    
+    // Start NSURLSession
+    NSURLSessionConfiguration *defaultConfigObject = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSURLSession *defaultSession = [NSURLSession sessionWithConfiguration: defaultConfigObject delegate: nil delegateQueue: [NSOperationQueue mainQueue]];
+    
+    // POST parameters
+    NSURL *url = [NSURL URLWithString:@"http://localhost:8888/promos/index.php"];
+    NSMutableURLRequest *urlRequest = [NSMutableURLRequest requestWithURL:url];
+    NSString *params = [NSString stringWithFormat:@"rw_app_id=1&code=%@&device_id=%@", code, uniqueIdentifier];
+    [urlRequest setHTTPMethod:@"POST"];
+    [urlRequest setHTTPBody:[params dataUsingEncoding:NSUTF8StringEncoding]];
+    
+    // NSURLSessionDataTask returns data, response, and error
+    NSURLSessionDataTask *dataTask =[defaultSession dataTaskWithRequest:urlRequest
+                                                      completionHandler:^(NSData *data,
+                                                                          NSURLResponse *response,
+                                                                          NSError *error) {
+                                                          
+                                                          // Remove progress window
+                                                          [MBProgressHUD hideHUDForView:self.view animated:YES];
+
+                                                          // Handle response
+                                                          NSHTTPURLResponse* httpResponse = (NSHTTPURLResponse*)response;
+                                                          NSInteger statusCode = [httpResponse statusCode];
+                                                          if(error == nil) {
+                                                              
+                                                              if (statusCode == 400) {
+                                                                  _textView.text = @"Invalid code";
+                                                              } else if (statusCode == 403) {
+                                                                  _textView.text = @"Code already used";
+                                                              } else if (statusCode == 200) {
+                                                                  
+                                                                  // Parse out the JSON data
+                                                                  NSError *jsonError;
+                                                                  NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data
+                                                                                                                       options:kNilOptions
+                                                                                                                         error:&jsonError];
+                                                                  
+                                                                  NSString* unlockCode = [json objectForKey:@"unlock_code"];
+                                                                  
+                                                                  // JSON data parsed, continue handling response
+                                                                  if ([unlockCode compare:@"com.razeware.test.unlock.cake"] == NSOrderedSame) {
+                                                                      _textView.text = @"The cake is a lie!";
+                                                                  } else {
+                                                                      _textView.text = [NSString stringWithFormat:@"Received unexpected unlock code: %@", unlockCode];
+                                                                  }
+                                                                  
+                                                              } else {
+                                                                  _textView.text = @"Unexpected error";
+                                                              }
+                                                          
+                                                          } else {
+                                                              _textView.text = error.localizedDescription;
+                                                          }
+                                                              
+                                                      }];
+
+    [dataTask resume];
+
+    return TRUE;
+
+}
 
 - (void)viewDidLoad
 {
